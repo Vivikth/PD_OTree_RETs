c0ae28b8d082e76e557c31993c86a76f54d60ea3
Vivikth <vivikth.narayanan3@gmail.com>
2021-04-30 22:18:25 +1000

    Tided up transcribing_lev1 code

--- transcribing_lev1/models.py
+++ transcribing_lev1/models.py
@@ -31,10 +31,10 @@ Real Effort Task. Type as many strings as possible.
 class Constants(BaseConstants):
     name_in_url = 'task_transcribing'
     players_per_group = None
-    task_timer = 120 #see Subsession, before_session_starts setting.
-
-    string_length = 4
-    characters = "ab" #Characters to create strings from.
+    # task_timer = 120 #see Subsession, before_session_starts setting.
+    #
+    # string_length = 4
+    # characters = "ab" #Characters to create strings from.
 
     num_rounds = 10 # must be more than the max one person can do in task_timer seconds
 
@@ -52,33 +52,14 @@ class Subsession(BaseSubsession):
     def creating_session(self):
 
         players = self.get_players()
-        if 'task_timer' in self.session.config:
-            task_timer = self.session.config['task_timer']
-        else:
-            task_timer = Constants.task_timer
 
         for p in self.get_players():
-            p.task_timer = task_timer
             p.correct_text = Constants.reference_texts[Constants.rand[self.round_number - 1]]
 
 class Group(BaseGroup):
 	pass
 
 class Player(BasePlayer):
-
-    def score_round(self):
-        # update player payoffs
-        if (self.correct_text == self.user_text):
-            self.is_correct = True
-            self.payoff_score = 1
-        else:
-            self.is_correct = False
-            self.payoff_score = c(0)
-
-
-    task_timer = models.PositiveIntegerField(
-        doc="""The length of the real effort task timer."""
-    )
 
     correct_text = models.CharField(
         doc="user's transcribed text")
@@ -88,9 +69,3 @@ class Player(BasePlayer):
     is_correct = models.BooleanField(
         doc="did the user get the task correct?")
 
-    ret_final_score = models.IntegerField(
-        doc="player's total score up to this round")
-
-    payoff_score = models.DecimalField(
-            doc = '''score in this task''', max_digits=6, decimal_places = 2
-        )
