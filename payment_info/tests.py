c6053d4a5ce73dff5c4b22f15cfe3029e1309a8a
Vivikth <vivikth.narayanan3@gmail.com>
2021-07-14 11:31:21 +1000

    Updated tests.py for payment_info

--- payment_info/tests.py
+++ payment_info/tests.py
@@ -1,11 +1,14 @@
 from . import *
 from otree.api import Bot
-
+from Global_Functions import global_cases
 
 
 class PlayerBot(Bot):
 
+    cases = global_cases
+
     def play_round(self):
-        if not self.player.participant.mobile:
+        if self.case['detect_mobile'] == 'non_mobile' and self.case['Ethics_Consent'] == 'Consent'\
+                and self.case['Introduction'] == 'all_correct':
             yield PaymentInfo, dict(first_name='Robot',
-                                last_name='Robot', uni_id='uROBOT', email_address='robot@robot.com')
+                                    last_name='Robot', uni_id='uROBOT', email_address='robot@robot.com')
