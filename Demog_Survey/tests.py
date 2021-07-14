f6fb7d1bfb5dc77bfcd5e75a3cca8e2e7a7e5ec2
Vivikth <vivikth.narayanan3@gmail.com>
2021-07-14 11:39:28 +1000

    Added tests.py for survey

--- Demog_Survey/tests.py
+++ Demog_Survey/tests.py
@@ -1,9 +1,14 @@
-from otree.api import Currency as c, currency_range
 from . import *
 from otree.api import Bot
-
+from Global_Functions import global_cases
 
 
 class PlayerBot(Bot):
+
+    cases = global_cases
+
     def play_round(self):
-        pass
+        if self.case['detect_mobile'] == 'non_mobile' and self.case['Ethics_Consent'] == 'Consent'\
+                and self.case['Introduction'] == 'all_correct':
+            yield Survey, dict(age=20, gender="Male", study="Business and Economics", econ_classes=5,
+                               years=4, GPA=5.5, identify="No")
