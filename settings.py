b1bc5242e0855b0ba970a5b3dc197a7b7e2189f6
Vivikth <vivikth.narayanan3@gmail.com>
2021-09-13 11:49:34 +1000

    Updated task names

--- settings.py
+++ settings.py
@@ -14,19 +14,19 @@ RET_LIST = ["RET_Choice"] + ["RET_Choice_2"] + Task1a_list + ["Menu_Select"] + T
 SESSION_CONFIGS = [
     dict(
         name='task_replication1a',
-        display_name="t1",
+        display_name="Replication Task",
         num_demo_participants=1,
         app_sequence=['task_replication1a'],
     ),
     dict(
         name='task_transcribing1a',
-        display_name="t2",
+        display_name="Interpretation Task",
         num_demo_participants=1,
         app_sequence=['task_transcribing1a'],
     ),
     dict(
         name='task_encoding1a',
-        display_name="t3",
+        display_name="Concealment Task",
         num_demo_participants=1,
         app_sequence=['task_encoding1a'],
     ),
@@ -38,13 +38,13 @@ SESSION_CONFIGS = [
     # ),
     dict(
         name='task_organising1a',
-        display_name="t4",
+        display_name="Organising Task",
         num_demo_participants=1,
         app_sequence=['task_organising1a'],
     ),
     dict(
         name='task_tabulation1a',
-        display_name="t5",
+        display_name="Tabulation Task",
         num_demo_participants=1,
         app_sequence=['task_tabulation1a'],
     ),
